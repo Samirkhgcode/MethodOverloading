04694088a31deb44c38bfa0353a051f274006ca2
Samirkhgcode <138569634+Samirkhgcode@users.noreply.github.com>
2023-07-18 02:06:04 +0400

    Method Overloading

---
+++ Method overloading/Method overloading/Program.cs
@@ -0,0 +1,212 @@
+﻿#region Classwork
+
+//int a = 5;
+
+//int b=a;
+
+//b = 100;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+//-------------------------------//
+
+
+//int[] arr1 = {1,2,3,4,5};
+
+//int[] arr2 = arr1;
+
+//arr2[0] = 100;
+
+//Console.WriteLine(arr1[0]);
+//Console.WriteLine(arr2[0]);
+
+
+
+//---------------------------------//
+
+
+
+//int a = 8;
+
+
+//Console.WriteLine(a);
+
+//Test(a);
+
+//static void Test(int a)
+//{
+//          a = 20;
+//        Console.WriteLine(a);
+//}
+
+
+//-----------------------------------//
+
+
+//int a = 8;
+
+
+//Console.WriteLine(a);
+
+//Test(ref a);
+
+//static void Test(ref int a)
+//{
+//    a = 20;
+//    Console.WriteLine(a);
+//}
+
+
+
+//-----------------------------//
+
+
+
+//int a = 8;
+
+
+//Console.WriteLine(a);
+
+//Test(out a);
+
+//static void Test(out int a)
+//{
+//    a = 20;
+//    Console.WriteLine(a);
+//}
+
+
+//-----------------------------------//
+
+
+
+//static void GetFullName(string name, string username)
+//{
+//    Console.WriteLine(name + " " + username);
+//}
+
+
+
+//-------------------------//
+
+
+
+
+
+
+//string name = "Samir";
+
+//Test(name);
+
+//Console.WriteLine(name);
+
+
+
+//static void Test(string name)
+//{
+
+//    name = "Xeyyam";
+//    Console.WriteLine(name);
+//}
+
+
+
+//-----------------------------------//
+
+
+
+//string name = "Pervin";
+
+
+//Console.WriteLine(name.Length);
+//Console.WriteLine(name.ToLower());
+
+//Console.WriteLine(name.Trim());
+
+
+
+//Console.WriteLine(name.Replace("e","a"));
+
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+//string name = "Pervin, S ala,m";
+
+
+//var arr = name.Split(",");
+//foreach (var item in arr)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+//-------------------------------------//
+
+
+//string name = "Salam Azerbaycan";
+
+//var arr =name.Split(" ");
+
+//string result = arr.ToString(); 
+
+
+//------------------//
+
+
+//string str1 = "C#";
+
+//string str2 = "Programing";
+
+//Console.WriteLine(str1+str2);
+
+//string result = string.Concat(str1, str2);
+
+//Console.WriteLine( "Joined string:" + result);
+
+//if (str1 == str2)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+//Console.WriteLine(str1.Substring(2));
+
+//----------------------//
+
+
+//string surname = "Filankesov";
+
+//Console.WriteLine(surname.Contains("s"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
